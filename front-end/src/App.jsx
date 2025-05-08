1773ab77345ef269d5d531319a05e993b752f63a
Stevensauro <31481885+Stevensauro@users.noreply.github.com>
2025-05-08 13:24:41 -0500

    Final update on file structure/template project files

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,14 @@
 import DemoComponent from "./components/DemoComponent/DemoComponent";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContainer from "./components/MainContainer";
 
 export default function App() {
-  return (
-    <>
-      <DemoComponent />
-    </>
-  );
+  return <>
+  <Footer/>
+    <MainContainer>
+      <DemoComponent/>
+    </MainContainer>
+  <Header/>
+  </>;
 }
