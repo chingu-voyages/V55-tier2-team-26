abfe9dd98178ab9ac7a8daeda6bd2f9c9a0d82fd
Stevensauro <31481885+Stevensauro@users.noreply.github.com>
2025-05-26 14:59:19 -0500

    Created routes for landing page and search

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,14 @@
+import { Outlet } from "react-router";
 import Header from "./components/Header";
 import MainContainer from "./components/MainContainer";
-import SearchBar from "./components/SearchBar";
 import Footer from "./components/Footer";
-import ResultsContainer from "./components/ResultsComponent/ResultsContainer";
 
 export default function App() {
   return (
     <>
       <Header />
       <MainContainer>
-        <SearchBar />
-        <ResultsContainer />
+        <Outlet />
       </MainContainer>
       <Footer />
     </>
--- front-end/src/Router.jsx
+++ front-end/src/Router.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import App from "./App";
 import ResultsPagination from "./components/ResultsComponent/ResultsPagination";
+import LandingPageLayout from "./components/LandingPageLayout";
+import ResultsPageLayout from "./components/ResultsPageLayout";
 
 export default function Router({ children }) {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" index element={<Navigate to="/search?page=1" replace/>}/>
-        <Route path="search">
-            <Route index element={<App/>}/>
-            <Route path=":page" element={<ResultsPagination/>}/>
+        <Route path="search" element={<Navigate to="/search" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/" element={<App />}>
+          <Route index element={<LandingPageLayout />} />
+          <Route path="search">
+            <Route index element={<ResultsPageLayout />} />
+            <Route path=":page" element={<ResultsPagination />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
