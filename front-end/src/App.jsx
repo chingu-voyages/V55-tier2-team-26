b8a959a25619f19da1d198e3c11fb99f78631498
Stevensauro <31481885+Stevensauro@users.noreply.github.com>
2025-05-29 09:21:09 -0500

    testing api

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,11 +3,12 @@ import Header from "./components/Header";
 import MainContainer from "./components/MainContainer";
 import Footer from "./components/Footer";
 
-import { sendChatResponse } from "./utils/gemini-api-utils";
+import { clearChatHistory, sendChatResponse } from "./utils/gemini-api-utils";
 
 export default function App() {
 
   sendChatResponse('test','hello').then(res=>console.log(res))
+  clearChatHistory()
 
   return (
     <>
--- front-end/src/utils/gemini-api-utils.js
+++ front-end/src/utils/gemini-api-utils.js
@@ -3,8 +3,6 @@ import process from "process";
 const sendChatResponse = async (e, userResponse) => {
   console.log(e, userResponse);
 
-  console.log(import.meta.env);
-
   const response = await fetch(
     "https://v55-tier2-team-26.onrender.com/chatbotai",
     {
@@ -24,4 +22,24 @@ const sendChatResponse = async (e, userResponse) => {
   console.log(response);
 };
 
-export { sendChatResponse };
+const clearChatHistory = async (e=null) => {
+  console.log(e);
+
+  const response = await fetch(
+    "https://v55-tier2-team-26.onrender.com/chatbotai",
+    {
+      method: "PUT",
+      headers: {
+        Origin: "https://deploy-preview-27--celebrated-bienenstitch-a518bd.netlify.app/",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta?.env.VITE_TOKEN || null}`,
+      },
+    }
+  );
+
+  console.log(response);
+};
+
+
+export { sendChatResponse, clearChatHistory };
