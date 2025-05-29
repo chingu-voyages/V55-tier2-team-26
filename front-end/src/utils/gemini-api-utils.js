419819bc61d14bdd0c9f09aaa729cca8cfa50dc3
Stevensauro <31481885+Stevensauro@users.noreply.github.com>
2025-05-29 08:43:25 -0500

    Update gemini-api-utils.js

--- front-end/src/utils/gemini-api-utils.js
+++ front-end/src/utils/gemini-api-utils.js
@@ -2,13 +2,14 @@ import process from "process";
 
 const sendChatResponse = async (e, userResponse) => {
   console.log(e, userResponse);
-  console.log(import.meta.env)
+
+  console.log(import.meta.env);
 
   const response = await fetch(
     "https://v55-tier2-team-26.onrender.com/chatbotai",
     {
       method: "POST",
-      headers: {    
+      headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
         Authorization: `Bearer ${import.meta?.env.VITE_TOKEN || null}`,
