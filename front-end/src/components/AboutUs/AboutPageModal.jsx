096b9aae6d3ccef9af9ec1e3db93eb39b0898156
Kristin Dailey <kristin.dailey02@gmail.com>
2025-06-03 12:00:56 -0700

    Refactor modal styling

--- front-end/src/components/AboutUs/AboutPageModal.jsx
+++ front-end/src/components/AboutUs/AboutPageModal.jsx
@@ -4,7 +4,7 @@ import CloseModalIcon from "./CloseModalIcon";
 export default function AboutPageModal({ handleOnCloseModal, ...props }) {
   return (
     <dialog className="max-w-svw w-svw max-h-svh h-svh" {...props}>
-      <section id="modal-header" className="relative">
+      <section className="relative">
         <img 
           src="../images/about-modal.jpg" 
           alt="Two blue chairs in front of a bookshelf full of books" 
@@ -15,43 +15,43 @@ export default function AboutPageModal({ handleOnCloseModal, ...props }) {
         </span>
       </section>
 
-      <section id="modal-body">
+      <section className="px-4">
         <img 
           src="../images/logo-placeholder.png" 
           alt="Resourcery logo" 
           className="h-auto w-1/2 mx-auto mt-3"
         />
-        <h1 className="text-center font-bold">Welcome to Resourcery</h1>
-        <hr className="ml-4 mr-4 border-t-4 border-[#998675] my-4 rounded"/>
-        <p className="ml-4 mr-4 mb-5">In today's fast-paced tech world, finding the right resource can feel like looking for a needle in a haystack. That's why we built Resourcery, a tool created by and for developers in the Chingu community.</p>
-        <p className="ml-4 mr-4 mb-5">Here, you can search through hand-picked technical resources shared in the #resource-treasures channel on Discord. Whether you're diving into JavaScript, exploring DevOps, or learning more about AI, Resourcery helps you find exactly what you need to level up.</p>
-        <h2 className="text-center font-semibold mt-10 mb-4">Features</h2>
+        <h1 className="text-center font-bold text-xl">Welcome to Resourcery</h1>
+        <hr className="border-t-4 border-[#998675] my-4 rounded"/>
+        <p className="mb-5">In today's fast-paced tech world, finding the right resource can feel like looking for a needle in a haystack. That's why we built Resourcery, a tool created by and for developers in the Chingu community.</p>
+        <p className="mb-5">Here, you can search through hand-picked technical resources shared in the #resource-treasures channel on Discord. Whether you're diving into JavaScript, exploring DevOps, or learning more about AI, Resourcery helps you find exactly what you need to level up.</p>
+        <h2 className="text-center font-semibold mt-10 mb-4 text-lg">Features</h2>
         <ul>
-          <li className="flex items-start gap-3 ml-4 mr-4 mb-2">
+          <li className="flex items-start gap-3 mb-2">
             <FaWandMagicSparkles className="mt-1 flex-shrink-0"/> 
             Use powerful features to search by tags or keywords.
           </li>
-          <li className="flex items-start gap-3 ml-4 mr-4 mb-2">
+          <li className="flex items-start gap-3 mb-2">
             <FaWandMagicSparkles className="mt-1 flex-shrink-0"/> 
             Discover articles, videos, and tools curated by real devs.
           </li>
-          <li className="flex items-start gap-3 ml-4 mr-4">
+          <li className="flex items-start gap-3">
             <FaWandMagicSparkles className="mt-1 flex-shrink-0"/> 
             <div>
               Ask our magical built-in AI assistant questions like: <span className="font-bold">"What topics can I explore here?"</span> or <span className="font-bold">"How do I search by author?"</span>
             </div>
           </li>
         </ul>
-        <p className="mt-5 ml-5 mr-5 text-center font-semibold">All curated. All searchable. All in one place. Let Resourcery help you find your next great learning moment.</p>
+        <p className="mt-5 ml-1 mr-1 text-center font-semibold">All curated. All searchable. All in one place. Let Resourcery help you find your next great learning moment.</p>
       </section>
 
-      <section id="modal-footer" className="bg-[#EDE1D4]">
-        <h3 className="text-center font-semibold pt-5 mt-10">Credits</h3>
+      <section className="bg-[#EDE1D4]">
+        <h3 className="text-center font-semibold pt-5 mt-10 text-lg">Credits</h3>
         <hr className="ml-4 mr-4 border-t-4 border-[#998675] my-4 mt-1 rounded"/>
         
-        <div className="credits-grid grid grid-cols-2 gap-4 px-6 pb-5">
+        <div className="credits-grid grid grid-cols-2 gap-4 px-6 pb-5 [&_h4]:font-semibold [&_h4]:mb-2">
           <div className="credits-column">
-            <h4 className="font-semibold mb-2">Developers</h4>
+            <h4>Developers</h4>
             <ul>
               <li>- Bryan Hoyem</li>
               <li>- Kristin Dailey</li>
@@ -61,21 +61,21 @@ export default function AboutPageModal({ handleOnCloseModal, ...props }) {
           </div>
 
           <div className="credits-column ml-4">
-            <h4 className="font-semibold mb-2">Designer</h4>
+            <h4>Designer</h4>
             <ul>
               <li>- Jessica Hackett</li>
             </ul>
           </div>
 
           <div className="credits-column">
-            <h4 className="font-semibold mb-2">Product Owner</h4>
+            <h4>Product Owner</h4>
             <ul>
               <li>- Xochitl Farias</li>
             </ul>
           </div>
 
           <div className="credits-column ml-4">
-            <h4 className="font-semibold mb-2">Scrum Masters</h4>
+            <h4>Scrum Masters</h4>
             <ul>
               <li>- Mikala Franks</li>
               <li>- Oghenerukevwe Egbaivwie</li>
