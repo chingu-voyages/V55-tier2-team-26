e6c8756def08ded3eb8fdeb0cdfa7a43dd0053a0
Kristin Dailey <kristin.dailey02@gmail.com>
2025-06-03 10:51:47 -0700

    Style modal body

--- front-end/src/components/AboutUs/AboutPageModal.jsx
+++ front-end/src/components/AboutUs/AboutPageModal.jsx
@@ -22,19 +22,31 @@ export default function AboutPageModal({ handleOnCloseModal, ...props }) {
           className="h-auto w-1/2 mx-auto mt-3"
         />
         <h1 className="text-center font-bold">Welcome to Resourcery</h1>
-        <p>In today's fast-paced tech world, finding the right resource can feel like looking for a needle in a haystack. That's why we built Resourcery, a tool created by and for developers in the Chingu community.</p>
-        <p>Here, you can search through hand-picked technical resources shared in the #resource-treasures channel on Discord. Whether you're diving into JavaScript, exploring DevOps, or learning more about AI, Resourcery helps you find exactly what you need to level up.</p>
-        <h2 className="text-center font-semibold">Features</h2>
+        <hr className="ml-4 mr-4 border-t-4 border-[#998675] my-4 rounded"/>
+        <p className="ml-4 mr-4 mb-5">In today's fast-paced tech world, finding the right resource can feel like looking for a needle in a haystack. That's why we built Resourcery, a tool created by and for developers in the Chingu community.</p>
+        <p className="ml-4 mr-4 mb-5">Here, you can search through hand-picked technical resources shared in the #resource-treasures channel on Discord. Whether you're diving into JavaScript, exploring DevOps, or learning more about AI, Resourcery helps you find exactly what you need to level up.</p>
+        <h2 className="text-center font-semibold mt-10 mb-4">Features</h2>
         <ul>
-          <li><FaWandMagicSparkles />Use powerful features to search by tags or keywords.</li>
-          <li><FaWandMagicSparkles />Discover articles, videos, and tools curated by real devs.</li>
-          <li><FaWandMagicSparkles />Ask our magical built-in AI assistant questions like: "What topics can I explore here?" or "How do I search by author?"</li>
+          <li className="flex items-start gap-3 ml-4 mr-4 mb-2">
+            <FaWandMagicSparkles className="mt-1 flex-shrink-0"/> 
+            Use powerful features to search by tags or keywords.
+          </li>
+          <li className="flex items-start gap-3 ml-4 mr-4 mb-2">
+            <FaWandMagicSparkles className="mt-1 flex-shrink-0"/> 
+            Discover articles, videos, and tools curated by real devs.
+          </li>
+          <li className="flex items-start gap-3 ml-4 mr-4">
+            <FaWandMagicSparkles className="mt-1 flex-shrink-0"/> 
+            <div>
+              Ask our magical built-in AI assistant questions like: <span className="font-bold">"What topics can I explore here?"</span> or <span className="font-bold">"How do I search by author?"</span>
+            </div>
+          </li>
         </ul>
-        <p>All curated. All searchable. All in one place. Let Resourcery help you find your next great learning moment.</p>
+        <p className="mt-5 ml-5 mr-5 text-center font-semibold">All curated. All searchable. All in one place. Let Resourcery help you find your next great learning moment.</p>
       </section>
 
       <section id="modal-footer">
-        <h3 className="text-center font-semibold">Credits</h3>
+        <h3 className="text-center font-semibold mt-10">Credits</h3>
         <h4>Developers</h4>
         <ul>
           <li>Bryan Hoyem</li>
