f5deec5bcdb23eda82cf8808129426f37353eeed
Kristin Dailey <kristin.dailey02@gmail.com>
2025-06-15 09:51:59 -0700

    Update hero styling per design

--- front-end/src/components/HeroArea.jsx
+++ front-end/src/components/HeroArea.jsx
@@ -1,13 +1,13 @@
-import logoImg from "../../images/logo.png"
+import logoImg from "../../images/resourcery_logo.png";
 
 export default function HeroArea() {
   return (
     <div className="flex flex-col items-center text-center bg-[#EDE0D4] text-black">
-      <div className="flex justify-center max-w-[200px] pt-14 pb-8">
-        <img src={logoImg} className="h-auto w-full max-w-[200px] rounded-full"/>
+      <div className="flex justify-center pt-14 pb-3">
+        <img src={logoImg} className="h-auto w-auto rounded-full"/>
       </div>
-      <h1 className="text-[30px] font-semibold">Resourcery</h1>
-      <h3 className="text-[14px] italic">Collected knowledge for tech professionals</h3>
+      <h1 className="font-syne font-bold text-3xl md:text-4xl">Resourcery</h1>
+      <h3 className="font-inter text-lg md:text-xl font-light italic mt-3 mb-3">Collected knowledge for tech <br/> professionals</h3>
     </div>
   );
 }
